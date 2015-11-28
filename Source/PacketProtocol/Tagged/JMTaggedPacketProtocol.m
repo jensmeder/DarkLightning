71cba8aa0db90c2244a2e6f553549ce1385fefe7
Jens Meder <kontakt@jensmeder.de>
2015-11-28 12:49:34 +0100

    JMTaggedPacketProtocol added

---
+++ Source/PacketProtocol/Tagged/JMTaggedPacketProtocol.m
@@ -0,0 +1,13 @@
+//
+//  JMTaggedPacketProtocol.m
+//  DarkLightning
+//
+//  Created by Jens Meder on 28/11/15.
+//  Copyright © 2015 Jens Meder. All rights reserved.
+//
+
+#import "JMTaggedPacketProtocol.h"
+
+@implementation JMTaggedPacketProtocol
+
+@end
